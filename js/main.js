f25ee94723e80b2549a3112318eb88507cf742ce
Encrpted Knowledge <davi.cunha.silva.ca@gmail.com>
2018-11-01 05:38:55 -0600

    index.html updated, typescript file updated.

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var releaseThumbnail = document.querySelectorAll(".releases-section .release-carousel .release-album-container #release-album-img img");
+releaseThumbnail.array.forEach(function (element) {
+    releaseThumbnail.addEventListener('click', function () {
+    });
+});
--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,7 @@
+let releaseThumbnail: any = document.querySelectorAll(".releases-section .release-carousel .release-album-container #release-album-img img");
 
+releaseThumbnail.array.forEach(element => {
+    releaseThumbnail.addEventListener('click', () => {
+
+    });
+});
